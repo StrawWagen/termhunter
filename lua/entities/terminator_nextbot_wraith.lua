4dde52af82b907cd629095d4c17a01a06c4684a7
StrawWagen <64710817+StrawWagen@users.noreply.github.com>
2025-05-04 11:32:27 -0700

    cleanup + dont shoot while invis

--- lua/entities/terminator_nextbot_wraith.lua
+++ lua/entities/terminator_nextbot_wraith.lua
@@ -11,21 +11,34 @@ list.Set( "NPC", "terminator_nextbot_wraith", {
     Class = "terminator_nextbot_wraith",
     Category = "Terminator Nextbot",
     Weapons = { "weapon_terminatorfists_term" },
+
 } )
 
 if CLIENT then
     language.Add( "terminator_nextbot_wraith", ENT.PrintName )
     return
+
 end
 
 -- cleaned :steamhappy:
 ENT.FistDamageMul = 1.565
 ENT.TERM_WEAPON_PROFICIENCY = WEAPON_PROFICIENCY_VERY_GOOD
 
+ENT.SpawnHealth = terminator_Extras.healthDefault * 0.5
+
+function ENT:CanWeaponPrimaryAttack()
+    if not self:IsSolid() then return false end
+    local nextAttack = self.terminator_NextAttack or 0
+    if nextAttack > CurTime() then return end
+    return BaseClass.CanWeaponPrimaryAttack( self )
+
+end
+
 -- copied from jerma modified & cleaned up a bit :) 
 function ENT:CloakedMatFlicker()
     local toApply = { self }
     table.Add( toApply, self:GetChildren() )
+
     for _, ent in pairs( toApply ) do
         if not IsValid( ent ) then continue end
         local entsParent = ent:GetParent()
@@ -33,6 +46,7 @@ function ENT:CloakedMatFlicker()
 
         if IsValid( ent ) then
             ent:SetMaterial( "effects/combineshield/comshieldwall3" )
+
         end
     end
     timer.Simple( math.Rand( 0.65, 0.75 ), function()
@@ -40,6 +54,7 @@ function ENT:CloakedMatFlicker()
         if self:IsSolid() then return end
         toApply = { self }
         table.Add( toApply, self:GetChildren() )
+
         for _, ent in pairs( toApply ) do
             if not IsValid( ent ) then continue end
             local entsParent = ent:GetParent()
@@ -47,6 +62,7 @@ function ENT:CloakedMatFlicker()
 
             if IsValid( ent ) then
                 ent:SetMaterial( "effects/combineshield/comshieldwall" )
+
             end
         end
     end )
@@ -73,17 +89,22 @@ function ENT:DoHiding( hide )
         table.Add( toApply, self:GetChildren() )
         for _, ent in pairs( toApply ) do
             if not IsValid( ent ) then continue end
+
             local entsParent = ent:GetParent()
             if ent ~= self and ( not IsValid( entsParent ) or entsParent ~= self ) then continue end
+
             ent:DrawShadow( false )
             ent:SetNotSolid( true )
+
         end
     else
         self:EmitSound( "ambient/levels/citadel/pod_close1.wav", 100, math.random( 125, 130 ) )
         self.terminator_NextHidingSwap = CurTime() + math.Rand( 2.5, 3.5 )
         self:CloakedMatFlicker()
+
         timer.Simple( 0.35, function()
             if not IsValid( self ) then return end
+
             self.terminator_NextAttack = CurTime() + 0.10
             self:EmitSound( "buttons/combine_button5.wav", 140, 125 )
             self:SetCollisionGroup( COLLISION_GROUP_NPC )
@@ -98,9 +119,11 @@ function ENT:DoHiding( hide )
                 if not IsValid( ent ) then continue end
                 local entsParent = ent:GetParent()
                 if ent ~= self and ( not IsValid( entsParent ) or entsParent ~= self ) then continue end
+
                 ent:DrawShadow( false )
                 ent:SetMaterial( "" )
                 ent:SetNotSolid( false )
+
             end
         end )
     end
@@ -113,14 +136,17 @@ function ENT:AdditionalThink()
     local enemDist = self.DistToEnemy
     if doHide and speedSqr < 85^1 then
         doHide = false
+
     elseif doHide and IsValid( enem ) and enemDist < 120 then
         doHide = false
         self:ReallyAnger( 90 ) -- ANGRY
+
     end
 
     self:DoHiding( doHide )
 
     if not self:IsSolid() and math.Rand( 0, 100 ) < 0.5 then
         self:CloakedMatFlicker()
+
     end
 end
